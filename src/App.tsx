47c6703def93fafb65b76a92eebdd4519ce9dd91
Wanyaxeli <eliwanyax@gmail.com>
2023-02-27 16:12:33 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
+import React,{useState,ChangeEvent,FormEvent} from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+interface Value {
+  first:string;
+  last:string;
+  password:string;
+  email:string;
+  phone:number
+}
 function App() {
+  const initialState:Value={
+   first:'',
+   last:'',
+   password:'',
+   email:'',
+   phone:2
+  }
+  const[value,setValue]=useState<Value>(initialState)
+  const handleChange=(e:ChangeEvent<HTMLInputElement>)=>{
+   const inputValue = e.target.value
+    const name = e.target.name
+    setValue({...value,[name]:inputValue})
+  }
+  const handleSubmit=(e:FormEvent)=>{
+   e.preventDefault()
+   alert ('submitted!')
+  }
+  console.log(value)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form>
+       <input name='first' onChange={handleChange} type='text' placeholder='First name'/>
+      <input name='last' onChange={handleChange} type='text' placeholder='Last name'/>
+      <input name='password' onChange={handleChange} type='password' placeholder='Password'/>
+      <input name='email' onChange={handleChange} type='email' placeholder='Email'/>
+      <input name='phone' pattern='[0-9]*' onChange={handleChange} type='text' placeholder='Phone number'/>
+      <button onClick={handleSubmit} type='submit'>Submit</button>
+    </form>
     </div>
   );
 }
